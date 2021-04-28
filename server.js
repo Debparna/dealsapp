8780d0db823c3677d3fd6624eacb8f420acfb324
Debparna <dpratiher@gmail.com>
2021-04-27 22:15:28 -0700

    k

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const config = require('config');
+
+const app = express();
+
+const db = config.get('mongoURI');
+
+mongoose
+  .connect(db, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false })
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
